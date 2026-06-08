da48846f8e91be421fcf46b77b37d95eec4dc9b8
Jaydeep Poojary <158280190+JaydeepPoojary@users.noreply.github.com>
2026-06-08 16:23:25 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c1a5e055c7364c05cb281dc3a13e94a8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "373ab3b959ba6c4ac894da8571e122f7",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8abf24d77e26f17b2561b145a1a96ef3",
+"flutter_bootstrap.js": "9565bfd53b682a6eb05cbd891ae48b7e",
 "icons/android-chrome-192x192.png": "15a5f483cac4875fb77f833fdb58c536",
 "icons/android-chrome-512x512.png": "5256612938de33d1eafb62d94b10d9fb",
 "icons/apple-touch-icon.png": "8de81ed4603f890a0d728e0877f27e6a",
@@ -44,9 +44,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c1a5e055c7364c05cb281dc3a13e94a8
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e5499175267235023906ae05b1589ef7",
-"/": "e5499175267235023906ae05b1589ef7",
-"main.dart.js": "b2e5e28451ed9bbe16df6677d2dd1033",
+"index.html": "148011f14d5f18ef7df321c654cc0c77",
+"/": "148011f14d5f18ef7df321c654cc0c77",
+"main.dart.js": "79a892d3405835783e3808c1bebf617c",
 "manifest.json": "ed85d0513cc449efa711a77686588e45",
 "version.json": "e0a8f68334a0b1ffb9f455b7c12fa5eb"};
 // The application shell files that are downloaded before a service worker can
